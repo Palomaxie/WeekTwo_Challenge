797966d23a4f26c6d1a00b5640692358de10fd16
Pearl Paloma <pearlpaloma@gmail.com>
2019-02-14 15:52:12 +0300

    Added a function

--- challengeTwo.py
+++ challengeTwo.py
@@ -4,9 +4,13 @@ result = int(input("Enter your year of Birth: "))
 
 age = (current_year - result)
 
-if age < 18:
-    print("you are a Minor!")
-elif age == 18 or age <= 36:
-    print("Your are a Youth")
-else:
-    print("You are an Elder")
+def my_age():
+
+    if age < 18:
+        print("you are a Minor!")
+    elif age == 18 or age <= 36:
+        print("Your are a Youth")
+    else:
+        print("You are an Elder")
+
+my_age()
